89c43a55cb368d8a7b8468097e6340785f3b68b7
廖均成 <2491562126@qq.com>
2022-12-12 07:31:02 +0800

    样式修改

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,20 +2,14 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/vuepress-note/",
+  
+  lang: "zh-CN",
 
-  locales: {
-    "/": {
-      lang: "en-US",
-      title: "Blog Demo",
-      description: "A blog demo for vuepress-theme-hope",
-    },
-    "/zh/": {
-      lang: "zh-CN",
-      title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
-    },
-  },
+  title: "Minnow",
+
+  description: "Minnow 的博客",
+
+  base: "/vuepress-note/",
 
   theme,
 
